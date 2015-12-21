0b3aa8217806451182faee66b041747f932cbb2e
Eduardo <eduardo_demoura@yahoo.com.br>
2015-12-21 01:47:34 -0200

    Update parseSitemap.js

--- parseSitemap.js
+++ parseSitemap.js
@@ -15,8 +15,16 @@ Draw.loadPlugin(function(ui) {
     */
     
     // Adds resources for actions
+    mxResources.parse('fromSitemap=From Sitemap');
     mxResources.parse('fromSitemapXmlFile=From Sitemap XML File');
-    mxResources.parse('fromSitemapText=From Sitemap Text');
+    
+    // Adds action : fromSitemap
+    ui.actions.addAction('fromSitemap', function() {
+    	ui.showDialog(new InsertSitemap(ui).container, 620, 420, true, true);
+    	ui.dialog.container.style.overflow = 'auto';
+    }, null, null, '');
+    
+    ui.keyHandler.bindAction(81, !0, 'fromSitemap', !0);
     
     // Adds action : fromSitemapXmlFile
     ui.actions.addAction('fromSitemapXmlFile', function() {
@@ -26,20 +34,12 @@ Draw.loadPlugin(function(ui) {
     
     ui.keyHandler.bindAction(81, !0, 'fromSitemapXmlFile', !0);
     
-    // Adds action : fromSitemapText
-    ui.actions.addAction('fromSitemapText', function() {
-    	ui.showDialog(new ParseDialog(ui).container, 620, 420, true, true);
-    	ui.dialog.container.style.overflow = 'auto';
-    }, null, null, '');
-    
-    ui.keyHandler.bindAction(81, !0, 'fromSitemapText', !0);
-    
     var uiMenus = ui.menus;
     
     // Adds menu
     ui.menubar.addMenu('Sitemap', function(menu, parent) {
-       uiMenus.addMenuItem(menu, 'fromSitemapText');
-       uiMenus.addMenuItem(menu, 'fromSitemapXmlFile');
+       uiMenus.addMenuItem(menu, 'fromSitemap');
+       //uiMenus.addMenuItem(menu, 'fromSitemapXmlFile');
     });
     
 	/* Replaces import from menu to add new menu item
@@ -60,4 +60,130 @@ Draw.loadPlugin(function(ui) {
                     
     // Displays status message
     ui.editor.setStatus('ParseSitemap plugin loaded successfully!');
+    
+    /**
+	 * Constructs a new dialog for sitemap insert.
+	 */
+	function InsertSitemap(editorUi) {
+		function parse(text) {
+			var lines = text.split('\n');
+			var vertices = new Object();
+			var cells = [];
+			
+			function getOrCreateVertex(id) {
+				var newVertex = vertices[id];
+	
+				if (newVertex == null) {
+					var label = id.match(/\/([^\/]+)\/[^\/]+$/);
+					newVertex = new mxCell(label, new mxGeometry(0, 0, 80, 30));
+					newVertex.vertex = true;
+					newVertex.setAttribute('link', id + '/');
+					vertices[id] = newVertex;
+					cells.push(newVertex);
+				}
+				
+				return newVertex;
+			};
+			
+			for (var i = 0; i < lines.length; i++) {
+				if (lines[i].charAt(0) != ';') {
+					
+					var values = lines[i].split('/'),
+						len = values.length;
+						
+					if (!values[len - 1]) {
+						values.slice(0, -1);
+					}
+					
+					if (len > 1) {
+						var target = getOrCreateVertex(values.join('/'));
+						values.slice(0, -1);
+						var source = getOrCreateVertex(values.join('/'));
+						
+						var newEdge = new mxCell('', new mxGeometry());
+						newEdge.edge = true;
+						source.insertEdge(newEdge, true);
+						target.insertEdge(newEdge, false);
+						cells.push(newEdge);
+					}
+				}
+			}
+			
+			if (cells.length > 0) {
+				var graph = editorUi.editor.graph;
+				
+				graph.getModel().beginUpdate();
+				try {
+					cells = graph.importCells(cells);
+					
+					for (var i = 0; i < cells.length; i++) {
+						if (graph.getModel().isVertex(cells[i])) {
+							var size = graph.getPreferredSizeForCell(cells[i]);
+							cells[i].geometry.width = Math.max(cells[i].geometry.width, size.width);
+							cells[i].geometry.height = Math.max(cells[i].geometry.height, size.height);
+						}
+					}
+	
+					var layout = new mxFastOrganicLayout(graph);
+					layout.disableEdgeStyle = false;
+					layout.forceConstant = 120;
+					layout.execute(graph.getDefaultParent());
+					
+					graph.moveCells(cells, 20, 20);
+				}
+				finally {
+					graph.getModel().endUpdate();
+				}
+			}
+		};
+		
+		var div = document.createElement('div');
+		div.style.textAlign = 'right';
+		
+		var textarea = document.createElement('textarea');
+		textarea.style.width = '600px';
+		textarea.style.height = '374px';
+		
+		textarea.value = ';example\nhttp://www.chrisplaw.com/bus-accidents/\nhttp://www.chrisplaw.com/car-accidents/\nhttp://www.chrisplaw.com/car-accidents/compensation/\nhttp://www.chrisplaw.com/car-accidents/what-do-i-need-to-do/\n';
+		div.appendChild(textarea);
+		
+		// Enables dropping files
+		if (fileSupport) {
+			function handleDrop(evt) {
+			    evt.stopPropagation();
+			    evt.preventDefault();
+			    
+			    if (evt.dataTransfer.files.length > 0) {
+			    	var file = evt.dataTransfer.files[0];
+	    			
+					var reader = new FileReader();
+					reader.onload = function(e) { textarea.value = e.target.result; };
+					reader.readAsText(file);
+	    		}
+			};
+			
+			function handleDragOver(evt) {
+				evt.stopPropagation();
+				evt.preventDefault();
+			};
+	
+			// Setup the dnd listeners.
+			textarea.addEventListener('dragover', handleDragOver, false);
+			textarea.addEventListener('drop', handleDrop, false);
+		}
+	
+		div.appendChild(mxUtils.button(mxResources.get("insert"), function() {
+			parse(textarea.value);
+			editorUi.hideDialog();
+		}));
+		
+		div.appendChild(mxUtils.button(mxResources.get('cancel'), function() {
+			evt.confirm(mxResources.get("areYouSure"), function() {
+                editorUi.hideDialog();
+            });
+		}));
+		
+		this.container = div;
+	};
+
 });
